02820c86bd3edcc7c924e471fefed43be6ad0bed
Ritik220519 <ritik220519@gmail.com>
2026-06-08 15:59:26 +0530

    created a server

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+
+app.use("/test" , ( req , res)=>{
+    res.send("here is first express server....")
+})
+app.use("/check" , ( req , res)=>{
+    res.send("here is second express route....")
+})
+
+
+
+app.listen(8268 , ()=>{
+    console.log("server is listing on port 8268")
+})
