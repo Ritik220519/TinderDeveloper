2f038479d05671fa115751d4e3a6d02c3db6905f
Ritik220519 <ritik220519@gmail.com>
2026-06-09 13:01:05 +0530

    multiple route handler req&res&next

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.use("/" , ( req , res)=>{
-    res.send("boom shakla k a")
+app.use("/" , ( req , res , next)=>{
+    console.log("request handler")
+    // res.send("handling the route")
+    next();
+},(req , res ,next)=>{
+    console.log("request handler 2")
+    // res.send("handling the route 2")
+    next();
+},(req , res)=>{
+    console.log("request handler 3")
+    res.send("handling the route 3")
+},(req , res)=>{
+    console.log("request handler 4")
+    res.send("handling the route 4")
 })
-app.use("/test" , ( req , res)=>{
-    res.send("here is first express server....")
-})
-app.use("/check" , ( req , res)=>{
-    res.send("here is second express route....")
-})
+
 
 
 
